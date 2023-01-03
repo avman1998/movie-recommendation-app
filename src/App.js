8fc478a30e36a2228b8610b578208e191d7bd1d3
Aman <avman1998@gmail.com>
2023-01-03 23:11:15 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Header } from "./components/Header";
-import { Categories } from "./components/Categories";
-import { Movies } from "./components/Movies";
+// import { Categories } from "./components/Categories";
+// import { Movies } from "./components/Movies";
 function App() {
   return (
     <div className="App  flex flex-col items-center  min-w-[100vw] min-h-[100vh] ">
