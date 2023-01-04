7ed1a5c7d561fc86134731df3393d56455461007
Aman <avman1998@gmail.com>
2023-01-04 23:11:18 +0530

    Category Component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Header } from "./components/Header";
-// import { Categories } from "./components/Categories";
+import { Categories } from "./components/Categories";
 // import { Movies } from "./components/Movies";
 function App() {
   return (
     <div className="App  flex flex-col items-center  min-w-[100vw] min-h-[100vh] ">
       <Header />
+      <Categories />
     </div>
   );
 }
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,55 @@
 import React from "react";
 
 export const Categories = () => {
-  return <div>Categories</div>;
+  const genres = [
+    {
+      name: "Action",
+      id: 28,
+    },
+    {
+      name: "Adventure",
+      id: 12,
+    },
+    {
+      name: "Animation",
+      id: 16,
+    },
+    {
+      name: "Comedy",
+      id: 35,
+    },
+    {
+      name: "Romance",
+      id: 10749,
+    },
+    {
+      name: "Horror",
+      id: 27,
+    },
+  ];
+  const genreBtnArray = genres.map((item) => {
+    return <CategoryBtn genre={item} />;
+  });
+  return (
+    <div className="flex flex-wrap md:flex-row flex-col  items-center justify-center gap-[40px] mt-[50px] ">
+      {genreBtnArray}
+    </div>
+  );
 };
+function CategoryBtn({ genre }) {
+  async function getData(id) {
+    console.log(id);
+    fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${id}&api_key=389d5483d89b4b005f9a423bae3fd0ce
+    `)
+      .then((res) => res.json())
+      .then((data) => console.log(data.results));
+  }
+  return (
+    <button
+      className="w-full  md:w-auto px-[30px] py-[10px] text-center font-bold text-[110%] bg-gray-300  rounded shadow-md shadow-gray-500 hover:text-gray-300 hover:bg-black hover:shadow-md hover:shadow-black hover:transition-shadow hover:duration-500"
+      onClick={() => getData(genre.id)}
+    >
+      {genre.name}
+    </button>
+  );
+}
