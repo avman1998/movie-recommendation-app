732420d6d50a83179a40fee849923bf113798b50
Aman <avman1998@gmail.com>
2023-01-08 00:59:24 +0530

    movie cards added

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,6 +1,8 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Movies } from "./Movies";
 export const Categories = () => {
+  const [movies, setMovies] = useState([]);
+  const [loader, setLoader] = useState();
   const genres = [
     {
       name: "Action",
@@ -35,29 +37,55 @@ export const Categories = () => {
       id: 27,
     },
   ];
+  async function getData(e, id) {
+    e.preventDefault();
+    console.log(id);
+    const url = `https://api.themoviedb.org/3/discover/movie?with_genres=${id}&api_key=389d5483d89b4b005f9a423bae3fd0ce`;
+    try {
+      setLoader(true);
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+      setLoader(false);
+    } catch (err) {
+      console.log(err);
+    }
+    // fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${id}&api_key=389d5483d89b4b005f9a423bae3fd0ce
+    // `)
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log(data.results);
+    //     setMovies(data.results);
+    //   });
+  }
   const genreBtnArray = genres.map((item) => {
-    return <CategoryBtn genre={item} />;
+    return (
+      <button
+        className=" w-[100px]  px-[5px] py-[10px] text-center font-bold text-[80%] bg-gray-300  rounded shadow-md shadow-gray-500 hover:text-gray-300 hover:bg-black hover:shadow-md hover:shadow-black hover:transition-shadow hover:duration-500 "
+        onClick={(e) => getData(e, item.id)}
+      >
+        {item.name}
+      </button>
+    );
   });
+
   return (
-    <div className="flex flex-wrap flex-row   items-center justify-center gap-[25px] mt-[20px] mb-[20px] ">
-      {genreBtnArray}
-    </div>
+    <>
+      <div className="flex flex-wrap flex-row   items-center justify-center gap-[25px] mt-[20px] mb-[20px] ">
+        {genreBtnArray}
+      </div>
+      {loader && <p>Loading</p>}
+      <Movies data={movies} />
+    </>
   );
 };
-function CategoryBtn({ genre }) {
-  async function getData(id) {
-    console.log(id);
-    fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${id}&api_key=389d5483d89b4b005f9a423bae3fd0ce
-    `)
-      .then((res) => res.json())
-      .then((data) => console.log(data.results));
-  }
-  return (
-    <button
-      className=" w-[100px]  px-[5px] py-[10px] text-center font-bold text-[80%] bg-gray-300  rounded shadow-md shadow-gray-500 hover:text-gray-300 hover:bg-black hover:shadow-md hover:shadow-black hover:transition-shadow hover:duration-500 "
-      onClick={() => getData(genre.id)}
-    >
-      {genre.name}
-    </button>
-  );
-}
+// function CategoryBtn({ genre }) {
+//   return (
+//     <button
+//       className=" w-[100px]  px-[5px] py-[10px] text-center font-bold text-[80%] bg-gray-300  rounded shadow-md shadow-gray-500 hover:text-gray-300 hover:bg-black hover:shadow-md hover:shadow-black hover:transition-shadow hover:duration-500 "
+//       onClick={() => getData(genre.id)}
+//     >
+//       {genre.name}
+//     </button>
+//   );
+// }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,5 +1,27 @@
 import React from "react";
 
-export const Movies = () => {
-  return <div>Movies</div>;
+export const Movies = ({ data }) => {
+  console.log(data);
+  const movieArr = data.map((item) => {
+    return <MovieCard info={item} />;
+  });
+  return (
+    <div className="flex flex-wrap gap-[10px] items-baseline justify-center">
+      {movieArr}
+    </div>
+  );
 };
+function MovieCard({ info }) {
+  return (
+    <div className="max-h-[300px] bg-black rounded max-w-[150px] flex flex-col items-center justify-center p-[10px] m-[10px] ggap-[5px] text-white">
+      <img
+        src={`https://www.themoviedb.org/t/p/w220_and_h330_face${info?.poster_path}`}
+        alt="img"
+      />
+      <div className="flex flex-col gap-[3px] text-[60%]">
+        <p>{info?.title}</p>
+        <p>{info?.vote_average} ⭐</p>
+      </div>
+    </div>
+  );
+}
